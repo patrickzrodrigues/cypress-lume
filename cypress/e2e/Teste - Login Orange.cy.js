3ba09a5054f1324d9209d95fdfb1bb7e58014eb1
patrickzrodrigues <patrick.z.rodriguess@gmail.com>
2026-02-12 17:17:42 -0300

    Add selectors list for login s

--- cypress/e2e/Teste - Login Orange.cy.js
+++ cypress/e2e/Teste - Login Orange.cy.js
@@ -1,5 +1,6 @@
-describe("Orange GRM Test", () => {
 
+import userData from "../fixtures/userData.json";
+describe("Orange GRM Test", () => {
   const selectorList = {
     username: "input[name='username']",
     password: "input[name='password']",
@@ -7,28 +8,33 @@ describe("Orange GRM Test", () => {
     CreadentialAlert: ".oxd-alert-content-text",
     SelectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
   };
+  const userData = {
+    userSucess: { userSucess: "Admin", password: "admin123" },
+    userFail: { userFail: "Admin", password: "234" },
+  };
+
   it.only("Login - sucesso", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
     cy.get(".orangehrm-login-branding").should("be.visible");
-    cy.get(selectorList.username).type("Admin");
-    cy.get(selectorList.password).type("admin123");
+    cy.get(selectorList.username).type(userData.userSucess.userSucess);
+    cy.get(selectorList.password).type(userData.userSucess.password);
     cy.get(selectorList.loginButton).click();
     cy.location("pathname").should("eq", "/web/index.php/dashboard/index");
-    cy.get(selectorList.SelectionTitleTopBar).should(
-      "contain",
-      "Dashboard",
-    );
+    cy.get(selectorList.SelectionTitleTopBar).should("contain", "Dashboard");
   });
   it.only("Login - Fail", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
     cy.get(".orangehrm-login-branding").should("be.visible");
-    cy.get(selectorList.username).type("Admin");
-    cy.get(selectorList.password).type("234");
+    cy.get(selectorList.username).type(userData.userFail.userFail);
+    cy.get(selectorList.password).type(userData.userFail.password);
     cy.get(selectorList.loginButton).click();
-    cy.get(selectorList.CreadentialAlert).should("contain", "Invalid credentials");
+    cy.get(selectorList.CreadentialAlert).should(
+      "contain",
+      "Invalid credentials",
+    );
   });
 });
